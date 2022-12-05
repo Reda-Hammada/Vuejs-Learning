14a209be1e3cc846282261f43f1c7a59ca9af03b
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-05 04:50:30 +0100

    example :value attribute and @input directive event with v-on:click example added

--- directives/app.js
+++ directives/app.js
@@ -3,14 +3,30 @@ const vm = Vue.createApp({
         return{
             firstName:'Reda',
             lastName:'Hammada',
+            age:20,
             url:'https://hammada-reda.com',
             raw_url:'<a href="https://hammada-reda.com" target="_blank">Reda Hammada</a>'
         }
     },
     methods:{
+
         fullName()
         {
             return `${this.firstName} ${this.lastName.toUpperCase()}`;
-        }
+        },
+        
+        updateLastName(event)
+        {
+           this.lastName  = event.target.value
+        },
+
+        increment()
+        {
+            this.age++
+        },
+        decrement()
+        {
+            this.age--
+        },
     }
 }).mount('#app')
