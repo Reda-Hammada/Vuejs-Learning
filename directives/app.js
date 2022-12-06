d7dddf9c4d88c59565c29256ff2fbf32d7694bbc
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-06 03:20:30 +0100

    v-model modifiers

--- directives/app.js
+++ directives/app.js
@@ -1,12 +1,14 @@
 const vm = Vue.createApp({
     data(){
         return{
+            
             firstName:'Reda',
             lastName:'Hammada',
             age:20,
-            occupation:'Vue js Developer',
             url:'https://hammada-reda.com',
-            raw_url:'<a href="https://hammada-reda.com" target="_blank">Reda Hammada</a>'
+            raw_url:'<a href="https://hammada-reda.com" target="_blank">Reda Hammada</a>',
+            occupation:'Vue js Developer',
+
         }
     },
     methods:{
