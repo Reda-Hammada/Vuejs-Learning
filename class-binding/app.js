f762e086c2d041b79acfb21e9908c222ba264a97
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-08 00:44:17 +0100

    update class binding examples

---
+++ class-binding/app.js
@@ -0,0 +1,15 @@
+const vm = Vue.createApp({
+    data(){
+        return{
+
+            isPurple:false,
+            selectedColor:'',
+        }
+    },
+    computed:{
+        circle_classes() {
+            return{ purple: this.isPurple }
+        }
+    }
+
+}).mount('#app')
