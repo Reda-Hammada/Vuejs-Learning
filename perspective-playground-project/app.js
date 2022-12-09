9f9acbe45bddb32d9891d47f1858a48bbdda340d
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-10 00:56:20 +0100

    transform property values set the reset method work

--- perspective-playground-project/app.js
+++ perspective-playground-project/app.js
@@ -2,27 +2,35 @@ let vm = Vue.createApp({
     data(){
         return{
 
+            perspective:0,
             rotateX:0,
             rotateZ:0,
             rotateY:0,
         }
-      
     },
     methods:{
+        
         reset(){
+            this.perspective=0,
             this.rotateX = 0
             this.rotateZ =0
             this. rotateY =0
         }
     },
     computed:{
-        
-        setRotate(){
-            
-            return `rotateX(${this.rotateX}deg)`
-            
+
+        setBox(){
+            return { transform:`
+                    perspective(${this.perspective}px)
+                    rotateX(${this.rotateX}deg)
+                    rotateY(${this.rotateY}deg)
+                    rotateZ(${this.rotateZ}deg)`
+
+                }
+            } 
         }
-    }
+        
+    
 }).mount('#app')
 
 
