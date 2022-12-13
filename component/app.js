7d87e71dac8366176554d1851c634acb86763b6b
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-13 03:21:30 +0100

    simple vue component example

---
+++ component/app.js
@@ -0,0 +1,13 @@
+let vm = Vue.createApp({
+ 
+})
+
+vm.component('Hello',{
+    template:`<h1>{{message}}</h1>`,
+    data(){
+        return{
+            message:'Hello Component'
+        }
+    }
+})
+vm.mount("#app")
