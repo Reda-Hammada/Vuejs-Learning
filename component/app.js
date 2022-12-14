4041398de2daa6bacb20f0a13387024ff99fae70
Reda Hammada <hammada.reda.dev@gmail.com>
2022-12-14 03:42:14 +0100

    vite added

--- component/app.js
+++ component/app.js
@@ -3,11 +3,17 @@ let vm = Vue.createApp({
 })
 
 vm.component('Hello',{
+
     template:`<h1>{{message}}</h1>`,
+
     data(){
+
         return{
+
             message:'Hello Component'
+            
         }
     }
 })
+
 vm.mount("#app")
