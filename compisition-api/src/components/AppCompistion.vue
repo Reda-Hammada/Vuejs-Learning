bf558b719d5cf40cd1e77d295feba248022b52d6
Reda Hammada <hammada.reda.dev@gmail.com>
2023-05-08 18:53:43 +0100

    update emit and props in composition api

--- compisition-api/src/components/AppCompistion.vue
+++ compisition-api/src/components/AppCompistion.vue
@@ -1,43 +1,81 @@
 <script>
-import { ref, reactive, toRefs, watchEffect,watch, computed } from "vue";
+import {
+  ref,
+  reactive,
+  toRefs,
+  watchEffect,
+  watch,
+  computed,
+  onBeforeMount,
+  onBeforeUpdate,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onMounted,
+} from "vue";
+import AppUser from "./AppUser.vue";
 
 export default {
   name: "AppCompistion",
+  components: {
+    AppUser,
+  },
   setup() {
-
     let num = ref(0);
 
     const user = reactive({
       name: "Reda",
       age: 22,
     });
+
     const increment = () => {
       num.value++;
+    };
+
+    const emitChangeUserName = () => {
+      user.name = "Adam";
     };
 
     setTimeout(() => {
       user.age = 23;
     }, 3000);
 
-     const renderReversedPhrae =  () => {
-        reveresedPhrase.value = phrase.value
+    const renderReversedPhrae = () => {
+      reveresedPhrase.value = phrase.value;
+    };
 
-     }
+    const phrase = ref("");
+    const reveresedPhrase = ref("");
 
-    const phrase  = ref('')
-    const reveresedPhrase  = ref('')
-    const double = computed(()=>{
-        return num.value * 2
-    })
-    // watcher 
-    watch([phrase], ([newVal,oldVal])=>{
-        renderReversedPhrae();
-    })
+    // computed
+    const double = computed(() => {
+      return num.value * 2;
+    });
+    // watcher
+    watch([phrase], ([newVal, oldVal]) => {
+      renderReversedPhrae();
+    });
 
     watchEffect(() => {
-         renderReversedPhrae();
-    })
+      renderReversedPhrae();
+    });
+    // lifecycle functions
+    // onBeforeMount();
+    onMounted(() => {
+      console.log("mounted");
+    });
+    // onBeforeUpdate();
+    // onUpdate();
+    // onBeforeUnmount() = beforeDestory();
+    // onUnmounted() = beforeDestory();
+    //      // additional ones
+    //      onActivated()
+    //      onDeactivated()
 
+    // // not supported
+    //     beforeCreate();
+    //     createImageBitmap();
 
     return {
       num,
@@ -46,24 +84,21 @@ export default {
       phrase,
       reveresedPhrase,
       double,
-
+      user,
+      emitChangeUserName,
     };
   },
 };
 </script>
 
 <template>
-  <p>{{ num }}</p>
-  <p>{{ name }}</p>
-  <p>{{ age }}</p>
+  <app-user :user="user" @emitChangeUserName="changeUserName"> </app-user>
 
   <button @click="increment">click me!</button>
-  
-  <input type="text"
-         v-model="phrase"
+  <button @click="emitChangeUserName">changer username !</button>
 
-         />
+  <input type="text" v-model="phrase" />
 
- {{ reveresedPhrase }}
- {{ double }}
+  {{ reveresedPhrase }}
+  {{ double }}
 </template>
---
+++ compisition-api/src/components/AppUser.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineProps } from "vue";
+
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup(props, context) {
+    context.emit("changeUserName");
+    const user = props.user;
+
+    return {
+      user,
+    };
+  },
+};
+</script>
+<template>
+  <p>
+    {{ user.name }}
+  </p>
+  <p>
+    {{ user.age }}
+  </p>
+</template>
